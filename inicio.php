c569f112ba3b2ccd2e1cedb5b4c4aec7f7f33f3d
Willx18 <josu4747@gmail.com>
2018-11-08 21:26:09 -0600

    version 1

---
+++ inicio.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="William Caballero">
+	<meta name="description" content="Codzilla el mejor editor de codigo en linea">
+	<meta name="keywords" content="codificar, codigo, html, javascript">
+
+
+	<link rel="stylesheet" href="css/estilo.css">
+	<link href="https://fonts.googleapis.com/css?family=Libre+Baskerville" rel="stylesheet">
+
+	<link rel="stylesheet" href="../bootstrap/bootstrap.min.css">
+
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+
+
+
+	
+	<title>CodZilla Login</title>
+
+</head>
+<body>
+
+	<section id="seccioninicio" class="row" >
+
+
+	<a href="inicio.php" id="logo" class="col-xl-5 col-lg-5 col-md-5 col-sm-5 col-xs-12" ><img src="img/logo1.png" id="logotipo" ></a>
+
+	<a href="../html/login.html" class="col-xl-3 col-lg-3 col-md-3 col-sm-2 col-xs-12 btn btnsesion " >Iniciar sesion</a>
+	<a href="../html/registro.html" class="col-xl-3 col-lg-3 col-md-3 col-sm-2 col-xs-12 btn btnsesion ">Registrarme</a>
+	
+
+
+	</section >
+
+
+	<nav id="navbar" >
+		<ul class="navbar">
+		<li class="nav-item col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12" ><a href="#" class="nav-link" style=" margin:0px">boton1</a> </li>
+		<li class="nav-item col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="#"class="nav-link "style=" margin:0px">boton2</a> </li>
+		<li class="nav-item col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="#"class="nav-link "style=" margin:0px">boton3</a> </li>
+		<li class="nav-item col-xl-3 col-lg-3 col-md-4 col-sm-6 col-xs-12"><a href="#"class="nav-link "style=" margin:0px">boton4</a> </li>
+		
+		</ul>
+	</nav>
+
+
+<div class="container">
+
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/maxresdefault (2).jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/paisajes-de-paris1.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/tipos-de-paisaje-natural.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+</div>
+	<footer></footer>
+	
+</body>
+</html>
